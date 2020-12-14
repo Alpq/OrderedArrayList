93075b9dad8e734c69e479965f8d09de898539cb
Alpq <doymazalp@gmail.com>
2020-12-13 19:34:44 -0500

    The NoNullArraylisy Class Compiles. The two constructors have been implemented and the parent add and set methods have been overidden.

---
+++ NoNullArrayList.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+public class NoNullArrayList<T> extends ArrayList<T>{
+  public NoNullArrayList()
+  {
+    super();
+  }
+  public NoNullArrayList(int size)
+  {
+    super(size);
+  }
+  public void add(int index, T element)
+  {
+    if (element != null)
+    {super.add(index, element);}
+    else
+    {throw new IllegalArgumentException();}
+  }
+  public boolean add(T element)
+  {
+    if (element == null) {throw new IllegalArgumentException();}
+    return super.add(element);
+  }
+  public T set(int index,T element)
+  {
+    if (element == null) {throw new IllegalArgumentException();}
+    return super.set(index, element);
+  }
+
+
+}
